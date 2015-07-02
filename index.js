fb5d2b235507151a870495160d163cb00551a8c2
reggi <thomas@reggi.com>
2015-07-02 00:42:33 -0400

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var Promise = require("bluebird")
+var cheerio = require("cheerio")
+var request = require("request-promise")
+
+request("https://docs.shopify.com/api")
+  .then(function(html){
+    var $ = cheerio.load(html)
+    var endpoints = $(".sidebar-api").children().last().find("ul li").map(function(i, el) {
+      return {
+        href: $(this).find("a").attr("href"),
+        text: $(this).text()
+      };
+    }).get()
+    return Promise.map(endpoints, function(endpoint){
+      return request("https://docs.shopify.com"+ endpoint.href)
+        .then(function(html){
+          var $ = cheerio.load(html)
+          endpoint.list = $(".api-endpointlist li")
+            .map(function(i, el) {
+              return {
+                request: $(this).find(".api-endpointlist-request-type").text(),
+                description: $(this).find(".api-endpointlist-description").text()
+              };
+            }).get()
+          return endpoint
+      })
+    })
+  })
+  .then(function(shopifyApi){
+    console.log(JSON.stringify(shopifyApi, null, 2))
+  })
